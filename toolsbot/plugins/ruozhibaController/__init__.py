f6a4f1555f38d87c8c6ad1a418a7b8304fc792fa
DKoTechnology <2733392694@qq.com>
2024-05-19 14:04:24 +0800

    添加 弱智吧 问题精选

--- toolsbot/plugins/ruozhibaController/__init__.py
+++ toolsbot/plugins/ruozhibaController/__init__.py
@@ -12,6 +12,7 @@ question = on_command("question", priority=5, block=True)
 
 @question.handle()
 async def _(args: Message = CommandArg()):
+    msg = ""
     msg += "弱智吧问题精选"
     questions:dict = eval(open("./ruozhiba_question.json", "r", encoding="utf-8"))
     msg += f"您抽到的问题为：{questions.keys()[randint(0,90)]}"
