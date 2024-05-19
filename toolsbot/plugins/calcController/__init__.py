6fb8c24180729817a558a943ac05b10485dfbd99
DKoTechnology <2733392694@qq.com>
2024-05-19 13:19:08 +0800

    更新 mainController

--- toolsbot/plugins/calcController/__init__.py
+++ toolsbot/plugins/calcController/__init__.py
@@ -8,17 +8,18 @@ import nonebot
 from time import sleep as wait
 from random import uniform as wrd
 
-calc_eventer = on_command("calc", priority=5, aliases=["计算器", "计算"])
+calc_eventer = on_command("calc", priority=5)
 
 @calc_eventer.handle()
 async def _(event: Message = CommandArg()):
-    msg = "\n简易计算器"
+    msg = "简易计算器\n"
     if event.extract_plain_text() == "":
-        msg += "\n请输入算式"
+        msg += "请输入算式"
         await calc_eventer.send(msg)
     else:
         try:
             expression = event.extract_plain_text()
+            msg += "结果：\n"
             if "+" in expression:
                 msg += str(eval(expression))
             elif "-" in expression:
